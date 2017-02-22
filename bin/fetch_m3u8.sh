1ea110e9b7df19a72872dff21b883e8df2e89f4c
hubin <hubin@3mang.com>
2017-02-23 06:07:51 +0800

    add fetch_m3u8.sh

---
+++ bin/fetch_m3u8.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#fetch m3u8
+
+echo fetching: $1
+OLD_IFS="$IFS" 
+IFS="/"
+array=($1) 
+IFS="$OLD_IFS" 
+
+
+num=${#array[@]} 
+
+echo $num
+
+m3u8=${array[num-1]}
+folder=${array[num-2]}
+
+mkdir $folder
+cd $folder
+path=""
+for ((i=0;i<num-1;i++))
+{
+	path=$path${array[i]}"/"
+}
+
+echo $path
+
+curl -i $1 -o $m3u8
+grep ts $m3u8 |while read LINE
+do
+    echo fetching: $path$LINE
+	curl -i $path$LINE -o $LINE
+done

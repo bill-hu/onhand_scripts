78460a62b33499fc60278fdb34aa405a2312e55a
hubin <hubin@3mang.com>
2019-01-07 19:10:55 +0800

    add build system_webview_apk in build_android_chrome.sh

--- bin/build_android_chrome.sh
+++ bin/build_android_chrome.sh
@@ -1,4 +1,4 @@
 . build/android/envsetup.sh
 build_log=build_$(date "+%y%m%d%H%M").log
 date >> $build_log 2>&1
-nohup ninja -C out/Default chrome_public_apk  >> $build_log 2>&1 &
+nohup ninja -C out/Default chrome_public_apk  system_webview_apk>> $build_log 2>&1 &
